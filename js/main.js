ea75c068a1b769a4da021d420476e5b581c94a06
Chambo015 <thegunner125@gmail.com>
2021-06-11 21:10:29 +0600

    Адаптив
    Адаптив страницы "О фильме"
    Добавил меню
    Добавил трейлер

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const menuButton = document.querySelector('.button-menu');
+const openMenu = document.querySelector('.nav-menu');
+const closeMenu = document.querySelector('.menu-close')
+
+menuButton.addEventListener('click', () => {
+  openMenu.classList.add('is-activ');
+  closeMenu.classList.add('is-activ');
+})
+
+closeMenu.addEventListener('click', () => {
+  openMenu.classList.remove('is-activ');
+  closeMenu.classList.remove('is-activ');
+})
