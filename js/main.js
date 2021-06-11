b413d313166447841490cecf37f988f381182e8d
Chambo015 <thegunner125@gmail.com>
2021-06-11 21:27:48 +0600

    попрвки в коде

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 
 const menuButton = document.querySelector('.button-menu');
 const openMenu = document.querySelector('.nav-menu');
-const closeMenu = document.querySelector('.menu-close')
+const closeMenu = document.querySelector('.menu-close');
 
 menuButton.addEventListener('click', () => {
   openMenu.classList.add('is-activ');
   closeMenu.classList.add('is-activ');
-})
+});
 
 closeMenu.addEventListener('click', () => {
   openMenu.classList.remove('is-activ');
   closeMenu.classList.remove('is-activ');
-})
+});
